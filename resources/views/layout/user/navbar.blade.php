2f6e61db2384666d792d1c561ee3d80eb5f82829
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-07-19 10:27:22 +0700

    update contact-us

--- resources/views/layout/user/navbar.blade.php
+++ resources/views/layout/user/navbar.blade.php
@@ -101,7 +101,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#"
+                    <a href="{{ route('contact-us', ['locale' => app()->getLocale()]) }}"
                         class="block py-2 px-3  {{ Route::currentRouteName() == 'contact-us' ? 'text-white bg-blue rounded md:bg-transparent md:text-blue md:p-0 dark:text-white md:dark:text-blue' : 'text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue md:p-0 dark:text-white md:dark:hover:text-blue dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent' }}">
                         Contact Us
                         @if (Route::currentRouteName() == 'contact-us')
