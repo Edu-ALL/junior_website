20512ce76eac3fba91cf109164cf4c5209c3c9af
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-07-25 10:56:12 +0700

    update meta

--- resources/views/layout/user/header.blade.php
+++ resources/views/layout/user/header.blade.php
@@ -5,10 +5,31 @@
     <title>
         @yield('title')
     </title>
-    <link rel="shortcut icon" href="{{asset('img/logo/favicon.png')}}" type="image/x-icon">
+
+    <meta name="title" content="EduALL Junior | One-stop solution to unlock  your kid’s potential " />
+    <meta name="description"
+        content="At EduALL Junior, we empower kids aged 5-12 to become future change-makers and leaders through our programs focused on developing an entrepreneurial mindset. Our courses are crafted to ignite curiosity, nurture creativity, and cultivate essential skills—all while ensuring a fun learning experience." />
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://junior.edu-all.com" />
+    <meta property="og:title" content="EduALL Junior | One-stop solution to unlock  your kid’s potential " />
+    <meta property="og:description"
+        content="At EduALL Junior, we empower kids aged 5-12 to become future change-makers and leaders through our programs focused on developing an entrepreneurial mindset. Our courses are crafted to ignite curiosity, nurture creativity, and cultivate essential skills—all while ensuring a fun learning experience." />
+    <meta property="og:image" content="{{asset('img/home/outstanding-skill.png')}}" />
+
+    <!-- Twitter -->
+    <meta property="twitter:card" content="summary_large_image" />
+    <meta property="twitter:url" content="https://junior.edu-all.com" />
+    <meta property="twitter:title" content="EduALL Junior | One-stop solution to unlock  your kid’s potential " />
+    <meta property="twitter:description"
+        content="At EduALL Junior, we empower kids aged 5-12 to become future change-makers and leaders through our programs focused on developing an entrepreneurial mindset. Our courses are crafted to ignite curiosity, nurture creativity, and cultivate essential skills—all while ensuring a fun learning experience." />
+    <meta property="twitter:image" content="{{asset('img/home/outstanding-skill.png')}}" />
+
+    <link rel="shortcut icon" href="{{ asset('img/logo/favicon.png') }}" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
-    
+
     <!-- Include Tailwind CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
