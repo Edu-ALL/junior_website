ae910cd068d4f1567ca578539da59b564902af52
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-08-08 15:05:03 +0700

    update fontawesome

--- resources/views/layout/user/footer.blade.php
+++ resources/views/layout/user/footer.blade.php
@@ -78,7 +78,7 @@
 </footer>
 
 <!--Fontawesome-->
-<script src="https://kit.fontawesome.com/d11faf3e43.js" crossorigin="anonymous"></script>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
 <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js"></script>
 
