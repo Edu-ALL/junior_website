0c5b9357b9ae04a8ece8375df17c8daf4c0bee96
Hafidz Annur Fanany <hafidz.bdt@gmail.com>
2024-09-10 11:10:59 +0700

    update pixel

--- resources/views/layout/user/header.blade.php
+++ resources/views/layout/user/header.blade.php
@@ -16,7 +16,7 @@
     <meta property="og:title" content="EduALL Junior | One-stop solution to unlock  your kid’s potential " />
     <meta property="og:description"
         content="At EduALL Junior, we empower kids aged 5-12 to become future change-makers and leaders through our programs focused on developing an entrepreneurial mindset. Our courses are crafted to ignite curiosity, nurture creativity, and cultivate essential skills—all while ensuring a fun learning experience." />
-    <meta property="og:image" content="{{asset('img/home/outstanding-skill.png')}}" />
+    <meta property="og:image" content="{{ asset('img/home/outstanding-skill.png') }}" />
 
     <!-- Twitter -->
     <meta property="twitter:card" content="summary_large_image" />
@@ -24,11 +24,39 @@
     <meta property="twitter:title" content="EduALL Junior | One-stop solution to unlock  your kid’s potential " />
     <meta property="twitter:description"
         content="At EduALL Junior, we empower kids aged 5-12 to become future change-makers and leaders through our programs focused on developing an entrepreneurial mindset. Our courses are crafted to ignite curiosity, nurture creativity, and cultivate essential skills—all while ensuring a fun learning experience." />
-    <meta property="twitter:image" content="{{asset('img/home/outstanding-skill.png')}}" />
+    <meta property="twitter:image" content="{{ asset('img/home/outstanding-skill.png') }}" />
 
     <link rel="shortcut icon" href="{{ asset('img/logo/favicon.png') }}" type="image/x-icon">
 
     <link href="https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide.min.css" rel="stylesheet">
+
+    <!-- Meta Pixel Code -->
+    <script>
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '521143860549431');
+        fbq('track', 'PageView');
+    </script>
+    <noscript>
+        <img height="1" width="1" style="" display:none src="https://www.facebook.com/tr?id=521143860549431&ev=PageView&noscript=1" />
+    </noscript>
+    <!-- End Meta Pixel Code -->
 
     <!-- Include Tailwind CSS -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
